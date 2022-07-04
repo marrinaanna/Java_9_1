c046dbef236a143e0616419af9bf3c1733659a54
Анна <marinaanna542@gmail.com>
2022-07-04 11:37:47 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.surname = "Иванов";
+        post.patronymic = "Иванович";
+        post.passport = "4444 № 44444444";
+        post.phone = "+7(999)999-99";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 6;
+        post.birthday.year = 1999;
+
+    }
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,10 @@
+public class Post {
+    public String name;
+    public String surname;
+    public String patronymic;
+    public String passport;
+    public String phone;
+    public boolean subscription;
+    public FormDate birthday;
+
+}
